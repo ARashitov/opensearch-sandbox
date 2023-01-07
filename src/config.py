8f3bab7eb948717a4ad392898e1443698b68b971
ARashitov <adil.rashitov.98@gmail.com>
2023-01-07 11:46:51 +0000

    feat: setup parametrization of KAFKA_POLL_TIMEOUT_MS, KAFKA_POLL_MAX_RECORDS, KAFKA_PRODUCER_SLEEP_SEC

--- src/config.py
+++ src/config.py
@@ -1,7 +1,12 @@
 import logging
+
+
 KAFKA_BOOTSTRAP_SERVERS = ["localhost:9092"]
 KAFKA_STREAM = "bets_to_register"
 KAFKA_CONSUMER_GROUP_ID = "bet_registration_consumers"
+KAFKA_POLL_TIMEOUT_MS = 20000
+KAFKA_POLL_MAX_RECORDS = 20000
+KAFKA_PRODUCER_SLEEP_SEC = 0.02
 
 logging.basicConfig(format='[ %(asctime)s ][ %(levelname)s ]: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
