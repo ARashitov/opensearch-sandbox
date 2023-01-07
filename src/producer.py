c202393605475d14ec8225f86eed8f2bdb0c714c
ARashitov <adil.rashitov.98@gmail.com>
2023-01-07 12:01:30 +0000

    fix: producer import

--- src/producer.py
+++ src/producer.py
@@ -1,13 +1,13 @@
 import os
 import sys
 
-from src import config
-from src.producer.main import handler
-from src.shared import io
-
-
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from src import config  # noqa: I100,E402
+# pylint: disable-next=E0611
+from src.producer.main import handler  # noqa: I100,E402
+from src.shared import io  # noqa: I100,E402
 
 
 if __name__ == "__main__":
@@ -17,4 +17,5 @@ if __name__ == "__main__":
         value_serializer=io.serialize,
         data_stream=config.KAFKA_STREAM,
         logger=logger,
+        producer_sleep_sec=config.KAFKA_PRODUCER_SLEEP_SEC,
     )
