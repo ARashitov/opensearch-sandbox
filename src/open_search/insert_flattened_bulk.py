006f5d872f18866fe1f56adc741f9842f79afea1
ARashitov <adil.rashitov.98@gmail.com>
2023-02-27 16:52:53 +0600

    setup bulk insertion with auth

--- src/open_search/insert_flattened_bulk.py
+++ src/open_search/insert_flattened_bulk.py
@@ -1,5 +1,3 @@
-from opensearchpy import OpenSearch
-
 import pandas as pd
 
 from src import config
@@ -24,17 +22,8 @@ def factory_bulk_index_action(
 
 if __name__ == "__main__":
 
-    os_client = OpenSearch(
-        hosts=settings.opensearch_hosts,
-        http_compress=True,
-        use_ssl=True,
-        verify_certs=False,
-        ssl_assert_hostname=False,
-        ssl_show_warn=False,
-    )
-
-    logger.info(os_client.info())
+    logger.info(config.client.info())
     bets = generators.generate_fake_bets(settings.amt_bets_to_generate)
     bets = pd.json_normalize(bets).to_dict("records")
     bulk_index_body = factory_bulk_index_action(bets, index=settings.index)
-    response = os_client.bulk(body=bulk_index_body)
+    response = config.client.bulk(body=bulk_index_body)
